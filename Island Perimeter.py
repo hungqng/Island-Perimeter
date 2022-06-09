7668e09d0092064b4c1b85dfffb62290c963f5e5
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-06-08 17:38:03 -0700

    Add files via upload

--- Island Perimeter.py
+++ Island Perimeter.py
@@ -13,3 +13,22 @@ class Solution:
             for i1, i2 in zip([0] + row, row + [0]):
                 area += int(i1 != i2)
         return area
+
+        # Solution 2
+        if not grid:
+            return 0
+
+        def sum_adjacent(i, j):
+            adjacent = (i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1),
+            res = 0
+            for x, y in adjacent:
+                if x < 0 or y < 0 or x == len(grid) or y == len(grid[0]) or grid[x][y] == 0:
+                    res += 1
+            return res
+
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    count += sum_adjacent(i, j)
+        return count
